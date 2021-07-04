811a6d64678f655d320dc60b88ffd9c03a077829
shk07 <seohk107@gmail.com>
2021-07-04 20:47:09 +0900

    habits(component+state)

--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -1,38 +1,41 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
-  state = {
-    count: 0,
-  };
-
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.props.onIncrement(this.props.habit);
   };
 
-  handleDcrement = () => {
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count });
+  handleDecrement = () => {
+    this.props.onDecrement(this.props.habit);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
   };
 
   render() {
+    const { name, count } = this.props.habit;
     return (
       <li className="habit">
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">{this.state.count}</span>
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
         <button
           className="habit-button habit-increase"
           onClick={this.handleIncrement}
         >
-          <i class="fas fa-plus-square"></i>
+          <i className="fas fa-plus-square"></i>
         </button>
         <button
           className="habit-button habit-decrease"
-          onClick={this.handleDcrement}
+          onClick={this.handleDecrement}
         >
-          <i class="fas fa-minus-square"></i>
+          <i className="fas fa-minus-square"></i>
         </button>
-        <button className="habit-button habit-delete">
-          <i class="fas fa-trash"></i>
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
+          <i className="fas fa-trash"></i>
         </button>
       </li>
     );
