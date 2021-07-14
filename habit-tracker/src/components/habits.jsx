64922e0787e5afa1010e0a9d2c1b3152f34e2118
shk07 <seohk107@gmail.com>
2021-07-14 22:23:43 +0900

    app+habits(reset-button)+app(css)

--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -18,6 +18,9 @@ class Habits extends Component {
             />
           ))}
         </ul>
+        <button className="habit-reset" onClick={this.props.onReset}>
+          Reset All
+        </button>
       </>
     );
   }
